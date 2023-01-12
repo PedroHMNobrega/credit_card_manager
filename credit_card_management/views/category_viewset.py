07dec6906ba3a6fb846d55cf8f1ca02818210a4f
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-01-12 18:56:12 -0300

    feat: define allowed http methods for category

--- credit_card_management/views/category_viewset.py
+++ credit_card_management/views/category_viewset.py
@@ -6,3 +6,4 @@ from credit_card_management.models import Category
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    http_method_names = ['get', 'post', 'delete']
