8ca833ce75e41755e062e82d79f9e76c38423d90
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-01-12 18:50:02 -0300

    feat: create category crud

---
+++ credit_card_management/views/category_viewset.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from credit_card_management.serializers import CategorySerializer
+from credit_card_management.models import Category
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
--- setup/settings.py
+++ setup/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'credit_card_management'
 ]
 
 MIDDLEWARE = [
