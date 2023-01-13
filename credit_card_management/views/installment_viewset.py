54c4128647943670477ca26607045d7bc4f8c1e4
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-01-13 11:49:31 -0300

    feat: only allow update installment paid and value_paid fields

--- credit_card_management/views/installment_viewset.py
+++ credit_card_management/views/installment_viewset.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound, ValidationError
 from credit_card_management.serializers import InstallmentSerializer
 from credit_card_management.models import Installment
 
@@ -11,3 +13,23 @@ class InstallmentViewSet(viewsets.ModelViewSet):
         user = self.request.user
         queryset = Installment.objects.filter(user_id=user.id)
         return queryset
+
+    def update(self, request, *args, **kwargs):
+        try:
+            user = request.user
+            installment_id = kwargs['pk']
+
+            installment = Installment.objects.get(pk=installment_id, user=user.id)
+
+            installment.paid = request.data['paid']
+            installment.value_paid = request.data['value_paid']
+
+            installment.save()
+
+            serializer = InstallmentSerializer(installment)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except KeyError:
+            raise ValidationError({'detail': 'Invalid input.'})
+        except:
+            raise NotFound()
