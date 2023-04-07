91d22390337e11cfa15c8193fa47f2aab39c058b
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-04-07 12:38:27 -0300

    feat: get category data on purchase list

--- credit_card_management/serializers/purchase_serializer.py
+++ credit_card_management/serializers/purchase_serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from credit_card_management.models import Purchase
+from credit_card_management.serializers import CategorySerializer
 
 
 class PurchaseSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
     class Meta:
         model = Purchase
         fields = ['id', 'name', 'installmentsNumber', 'value', 'category', 'firstInstallmentDate']
