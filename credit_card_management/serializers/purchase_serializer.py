b3c980ac58ae88690d075dbf3e2d5671a69e8935
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-04-07 12:46:53 -0300

    Revert "feat: get category data on purchase list"
    This reverts commit 91d22390337e11cfa15c8193fa47f2aab39c058b.

--- credit_card_management/serializers/purchase_serializer.py
+++ credit_card_management/serializers/purchase_serializer.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from credit_card_management.models import Purchase
-from credit_card_management.serializers import CategorySerializer
 
 
 class PurchaseSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-
     class Meta:
         model = Purchase
         fields = ['id', 'name', 'installmentsNumber', 'value', 'category', 'firstInstallmentDate']
