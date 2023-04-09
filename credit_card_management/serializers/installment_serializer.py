756dd73c7d82a1a3a1454578890c8eb3df79aef8
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-04-09 14:41:37 -0300

    feat: get purchase data on installment list

--- credit_card_management/serializers/installment_serializer.py
+++ credit_card_management/serializers/installment_serializer.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from credit_card_management.models import Installment
+from credit_card_management.serializers import PurchaseSerializer
 
 
 class InstallmentSerializer(serializers.ModelSerializer):
+    purchase = PurchaseSerializer()
+    category = serializers.CharField(source='purchase.category')
+
     class Meta:
         model = Installment
-        fields = ['id', 'purchase', 'number', 'value_paid', 'date', 'paid']
+        fields = ['id', 'purchase', 'number', 'value_paid', 'date', 'paid', 'category']
